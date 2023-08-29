f97af78e42ccf4107d2a55fbffa6945f0658ee23
CilginSinek <CilginSinek58@gmail.com>
2023-08-29 19:41:08 +0300

    try and change middleware

--- ImageHandler.ts
+++ ImageHandler.ts
@@ -8,42 +8,42 @@ export const config = {
 const baseUrl = "https://vue-portfolio-taupe.vercel.app"
 
 export default async function handler(request: Request, context:RequestContext) {
-    if(request.method ==="GET"){
-        if(request.url ===baseUrl+"/Icons/discord.png"){
-            const imageData = await fetch(new URL('./Icons/discord.png', import.meta.url)).then(
-                (res) => res.arrayBuffer(),
-            );
-            new ImageResponse((
-                <image width="96px" height="96px" src={imageData}/>
-            ))
-        }else if(request.url === baseUrl +"/Icons/github.png"){
-            const imageData = await fetch(new URL('./Icons/github.png', import.meta.url)).then(
-                (res) => res.arrayBuffer(),
-            );
-            new ImageResponse((
-                <image width="96px" height="96px" src={imageData}/>
-            ))
-        }else if(request.url === baseUrl +"/Icons/linkedin.png"){
-            const imageData = await fetch(new URL('./Icons/linkedin.png', import.meta.url)).then(
-                (res) => res.arrayBuffer(),
-            );
-            new ImageResponse((
-                <image width="96px" height="96px" src={imageData}/>
-            ))
-        }else if(request.url === baseUrl +"/Icons/twitter.png"){
-            const imageData = await fetch(new URL('./Icons/twitter.png', import.meta.url)).then(
-                (res) => res.arrayBuffer(),
-            );
-            new ImageResponse((
-                <image width="96px" height="96px" src={imageData}/>
-            ))
-        }else{
-            const imageData = await fetch(new URL('./Icons/null.png', import.meta.url)).then(
-                (res) => res.arrayBuffer(),
-            );
-            new ImageResponse((
-                <image width="96px" height="96px" src={imageData}/>
-            ))
-        }
-    }
+    // if(request.method ==="GET"){
+    //     if(request.url ===baseUrl+"/api/discord.png"){
+    //         const imageData = await fetch(new URL('./Icons/discord.png', import.meta.url)).then(
+    //             (res) => res.arrayBuffer(),
+    //         );
+    //         new ImageResponse((
+    //             <image width="96px" height="96px" src={imageData}/>
+    //         ))
+    //     }else if(request.url === baseUrl +"/api/github.png"){
+    //         const imageData = await fetch(new URL('./Icons/github.png', import.meta.url)).then(
+    //             (res) => res.arrayBuffer(),
+    //         );
+    //         new ImageResponse((
+    //             <image width="96px" height="96px" src={imageData}/>
+    //         ))
+    //     }else if(request.url === baseUrl +"/api/linkedin.png"){
+    //         const imageData = await fetch(new URL('./Icons/linkedin.png', import.meta.url)).then(
+    //             (res) => res.arrayBuffer(),
+    //         );
+    //         new ImageResponse((
+    //             <image width="96px" height="96px" src={imageData}/>
+    //         ))
+    //     }else if(request.url === baseUrl +"/api/twitter.png"){
+    //         const imageData = await fetch(new URL('./Icons/twitter.png', import.meta.url)).then(
+    //             (res) => res.arrayBuffer(),
+    //         );
+    //         new ImageResponse((
+    //             <image width="96px" height="96px" src={imageData}/>
+    //         ))
+    //     }else{
+    //         const imageData = await fetch(new URL('./Icons/null.png', import.meta.url)).then(
+    //             (res) => res.arrayBuffer(),
+    //         );
+    //         new ImageResponse((
+    //             <image width="96px" height="96px" src={imageData}/>
+    //         ))
+    //     }
+    // }
 }
