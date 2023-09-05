8f365f5e900b90c40f8836ca62042405ca8094b1
CilginSinek <CilginSinek58@gmail.com>
2023-09-05 15:26:54 +0300

    Deleted ugly markdown-vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import VueMarkdown from 'markdown-vue';
 
-const about = ref<string>("");
+const aboutArray = ref<string[]>([]);
 onMounted(async () => {
     await fetch(`/api/about`,{
         method:"GET",
         mode:"same-origin",
         credentials: "same-origin"
     }).then(response => response.json()).then(res => {
-        about.value = res
-    }).catch(err => about.value = err.message)
+        aboutArray.value = res.split("\n");
+    }).catch(err => aboutArray.value = [err.message])
 })
 
 </script>
 
 <template>
     <div class="card">
-        <VueMarkdown class="aboutText" :source="about ? about : '# Loading...'" />
+        <div v-if="aboutArray.length" class="aboutText">
+            <p v-for="about in aboutArray">
+                {{ about }}
+            </p>
+        </div>
+        <div v-else>
+            <p>Loading...</p>
+        </div>
     </div>
 </template>
